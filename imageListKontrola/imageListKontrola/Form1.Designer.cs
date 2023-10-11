0b8eb04b4b42a3a001316d0496f48491a23c4a72
Non't <90344104+Shef905015@users.noreply.github.com>
2023-10-11 09:21:54 +0200

    Add files via upload

---
+++ imageListKontrola/imageListKontrola/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace imageListKontrola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnPromjeni = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 386);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btnPromjeni
+            // 
+            this.btnPromjeni.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnPromjeni.Location = new System.Drawing.Point(294, 405);
+            this.btnPromjeni.Name = "btnPromjeni";
+            this.btnPromjeni.Size = new System.Drawing.Size(194, 41);
+            this.btnPromjeni.TabIndex = 1;
+            this.btnPromjeni.Text = "Promjeni sliku!";
+            this.btnPromjeni.UseVisualStyleBackColor = true;
+            this.btnPromjeni.Click += new System.EventHandler(this.btnPromjeni_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPromjeni);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnPromjeni;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ imageListKontrola/imageListKontrola/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace imageListKontrola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPromjeni_Click(object sender, EventArgs e)
+        {
+            int brojac = 0;
+                if(brojac == 0)
+            {
+
+                pictureBox1.Image = imageList1.Images[0];
+                brojac++;
+
+            }
+            if (brojac == 1)
+            {
+
+                pictureBox1.Image = imageList1.Images[1];
+                brojac++;
+
+            }
+            if (brojac == 2)
+            {
+
+                pictureBox1.Image = imageList1.Images[2];
+                brojac++;
+
+            }
+            if (brojac == 3)
+            {
+
+                pictureBox1.Image = imageList1.Images[3];
+                brojac = 0;
+
+            }
+
+        }
+    }
+}
